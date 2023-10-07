79ae92b08a320e992a06c9740d3c48808476065e
Christian Arzu <linkarzu@gmail.com>
2023-10-07 12:48:06 -0600

    added hostname to opt winbar

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -1,4 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.winbar = "%=%m %f"
+
+-- This add the bar that shows the file path on the top right
+-- vim.opt.winbar = "%=%m %f"
+-- Modified version of the bar that shows the hostname
+vim.opt.winbar = "%=" .. vim.fn.systemlist("hostname")[1] .. "         %m %f"
