7c09624fedad358bc10cda22eced750c6759b8c2
Christian Arzu <linkarzu@gmail.com>
2023-10-08 06:23:59 -0600

    changed hostname fg_color on lualine to dark gray

--- nvim-lazyvim/lua/plugins/lualine.lua
+++ nvim-lazyvim/lua/plugins/lualine.lua
@@ -18,21 +18,31 @@ return {
     local function decide_color()
       local hostname = vim.fn.systemlist("hostname")[1]
       local last_char = string.sub(hostname, -1)
+      local bg_color
+      local fg_color = "#36454F" -- default dark gray
+
       if last_char == "1" then
-        return "#0DFFAE"
+        bg_color = "#0DFFAE"
+        -- fg_color = "#000080"  -- dark blue
       elseif last_char == "2" then
-        return "#FF6200"
+        bg_color = "#FF6200"
+        -- fg_color = "#000080"  -- dark blue
       elseif last_char == "3" then
-        return "#DBF227"
+        bg_color = "#DBF227"
+        -- fg_color = "#000080"  -- dark blue
       else
-        return "#FF69B4" -- default
+        bg_color = "#FF69B4" -- default pink
       end
+
+      return bg_color, fg_color
     end
+
+    local bg_color, fg_color = decide_color()
 
     -- Adding hostname to lualine_x
     table.insert(opts.sections.lualine_x, 1, {
       "hostname",
-      color = { fg = "#FFFFFF", bg = decide_color() },
+      color = { fg = fg_color, bg = bg_color, gui = "bold" },
       -- section_separators = { left = "", right = "" },
       -- component_separators = { left = "", right = "" },
     })
