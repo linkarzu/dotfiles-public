935c156016a684324c47dcb8d9e716fb804708c9
Christian Arzu <linkarzu@gmail.com>
2023-10-08 06:11:10 -0600

    added space so dont shown on tree

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -5,4 +5,4 @@
 -- This add the bar that shows the file path on the top right
 -- vim.opt.winbar = "%=%m %f"
 -- Modified version of the bar that shows the hostname
-vim.opt.winbar = "%=" .. vim.fn.systemlist("hostname")[1] .. "         %m %f"
+vim.opt.winbar = "%=" .. vim.fn.systemlist("hostname")[1] .. "            %m %f"
