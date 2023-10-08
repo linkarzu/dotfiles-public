cdaf73fbe890a81b8ee29a5745bdba62abd94f24
Christian Arzu <linkarzu@gmail.com>
2023-10-08 06:26:31 -0600

    deleted old commented code

--- nvim-lazyvim/lua/plugins/lualine.lua
+++ nvim-lazyvim/lua/plugins/lualine.lua
@@ -1,16 +1,3 @@
--- return {
---   "nvim-lualine/lualine.nvim",
---   opts = function(_, opts)
---     -- Adding hostname to lualine_x
---     table.insert(opts.sections.lualine_x, 1, {
---       "hostname",
---       color = { fg = "#FFFFFF", bg = "#FF69B4" }, -- White text on pink background
---       -- section_separators = { left = "", right = "" },
---       -- component_separators = { left = "", right = "" },
---     })
---   end,
--- }
-
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
