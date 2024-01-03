76d903027b3656cfa6e2eb578b8f374af7f07a1c
Christian Arzu <linkarzu@gmail.com>
2024-01-03 16:45:00 -0600

    added prettier for markdown formatting

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
   },
   defaults = {
