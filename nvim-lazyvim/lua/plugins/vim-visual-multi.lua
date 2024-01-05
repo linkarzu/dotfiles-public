231a51dfb1dbe83f0527d417527e9f4f2df44471
Christian Arzu <linkarzu@gmail.com>
2024-01-05 14:06:36 -0600

    added plugin

---
+++ nvim-lazyvim/lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,10 @@
+-- https://github.com/mg979/vim-visual-multi
+
+-- In macos disable Settings - Keyboard - Keyboard Shortcuts
+-- Disable both 'Mission Control' shortcuts that use ctrl+down and ctrl+up
+-- As those are used to create multiple cursors
+
+return {
+  -- add gruvbox
+  { "mg979/vim-visual-multi" },
+}
