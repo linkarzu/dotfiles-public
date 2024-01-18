eba735156c57b3387045450c4d6c8fcf3dbf96a9
Christian Arzu <linkarzu@gmail.com>
2024-01-18 13:10:39 -0600

    create symlink backup if a file exists

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -1,5 +1,11 @@
 # Filename: /Users/krishna/github/dotfiles-public/zshrc/zshrc-file.sh
 
+# #############################################################################
+# Do not delete the `UNIQUE_ID` line below, I use it to backup original files
+# so they're not lost when my symlinks are applied
+# UNIQUE_ID=do_not_delete_this_line
+# #############################################################################
+
 boldGreen="\033[1;32m"
 boldYellow="\033[1;33m"
 boldRed="\033[1;31m"
@@ -10,24 +16,90 @@ noColor="\033[0m"
 # Run a clear command right after I log in to any host
 clear
 
+# ~/.config is used by neovim, alacritty and karabiner
+mkdir -p ~/.config
+# Alacritty is inside its own dir
+mkdir -p ~/.config/alacritty
+
 # Create the symlinks I normally use
 # ~/.config dir holds nvim, neofetch, alacritty configs
 # If the dir/file that the symlink points to doesnt exist, it will error out, so I direct them to dev null
-mkdir -p ~/.config
-mkdir -p ~/.config/alacritty
-ln -snf ~/github/dotfiles-public/vimrc/vimrc-file ~/.vimrc >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/vimrc/vimrc-file ~/github/obsidian_main/.obsidian.vimrc >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/zshrc/zshrc-file.sh ~/.zshrc >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/tmux/tmux.conf ~/.tmux.conf >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/neovim/nvim-lazyvim ~/.config/nvim >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/yabai/yabairc ~/.yabairc >/dev/null 2>&1
+# This will update the symlink even if its pointing to another file
+# If the file exists, it will create a backup in the same dir
+create_symlink() {
+	local source_path=$1
+	local target_path=$2
+	local backup_needed=true
 
-ln -snf ~/github/dotfiles-public/hammerspoon ~/.hammerspoon >/dev/null 2>&1
-ln -snf ~/github/dotfiles-public/karabiner ~/.config/karabiner >/dev/null 2>&1
+	# echo
+
+	# Check if the target is a file and contains the unique identifier
+	if [ -f "$target_path" ] && grep -q "UNIQUE_ID=do_not_delete_this_line" "$target_path"; then
+		# echo "$target_path is a FILE and contains UNIQUE_ID"
+		backup_needed=false
+	fi
+
+	# Check if the target is a directory and contains the UNIQUE_ID.sh file with the unique identifier
+	if [ -d "$target_path" ] && [ -f "$target_path/UNIQUE_ID.sh" ]; then
+		if grep -q "UNIQUE_ID=do_not_delete_this_line" "$target_path/UNIQUE_ID.sh"; then
+			# echo "$target_path is a DIRECTORY and contains UNIQUE_ID"
+			backup_needed=false
+		fi
+	fi
+	# Check if symlink already exists and points to the correct source
+	if [ -L "$target_path" ]; then
+		if [ "$(readlink "$target_path")" = "$source_path" ]; then
+			# echo "$target_path exists and is correct, no action needed"
+			return 0
+		else
+			echo -e "${boldYellow}'$target_path' is a symlink"
+			echo -e "but it points to a different source, updating it${noColor}"
+		fi
+	fi
+
+	# Backup the target if it's not a symlink and backup is needed
+	if [ -e "$target_path" ] && [ ! -L "$target_path" ] && [ "$backup_needed" = true ]; then
+		local backup_path="${target_path}_backup_$(date +%Y%m%d%H%M%S)"
+		echo -e "${boldYellow}Backing up your existing file '$target_path' to '$backup_path'${noColor}"
+		mv "$target_path" "$backup_path"
+	fi
+
+	# Create the symlink and print message
+	ln -snf "$source_path" "$target_path"
+	echo -e "${boldPurple}Created or updated symlink"
+	echo -e "${boldGreen}FROM: '$source_path'"
+	echo -e "  TO: '$target_path'${noColor}"
+}
+
+# Creating symlinks for files and directories
+create_symlink ~/github/dotfiles-public/vimrc/vimrc-file ~/.vimrc
+create_symlink ~/github/dotfiles-public/vimrc/vimrc-file ~/github/obsidian_main/.obsidian.vimrc
+create_symlink ~/github/dotfiles-public/zshrc/zshrc-file.sh ~/.zshrc
+create_symlink ~/github/dotfiles-public/tmux/tmux.conf ~/.tmux.conf
+create_symlink ~/github/dotfiles-public/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+create_symlink ~/github/dotfiles-public/yabai/yabairc ~/.yabairc
+
+# Creating symlinks for directories
+create_symlink ~/github/dotfiles-public/neovim/nvim-lazyvim ~/.config/nvim
+create_symlink ~/github/dotfiles-public/hammerspoon ~/.hammerspoon
+create_symlink ~/github/dotfiles-public/karabiner/mxstbr ~/.config/karabiner
+
 # # This is on the other repo where I keep my ssh config files
 # I commented this as I don't have access to this repo in all the hosts
 # ln -snf ~/github/dotfiles/sshconfig-pers ~/.ssh/config 2>&1 >/dev/null
+
+# I'm keeping the old manual commands here
+# ln -snf ~/github/dotfiles-public/vimrc/vimrc-file ~/.vimrc >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/vimrc/vimrc-file ~/github/obsidian_main/.obsidian.vimrc >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/zshrc/zshrc-file.sh ~/.zshrc >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/tmux/tmux.conf ~/.tmux.conf >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/yabai/yabairc ~/.yabairc >/dev/null 2>&1
+#
+# # Below are symlinks that point to directories
+# ln -snf ~/github/dotfiles-public/neovim/nvim-lazyvim ~/.config/nvim >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/hammerspoon ~/.hammerspoon >/dev/null 2>&1
+# ln -snf ~/github/dotfiles-public/karabiner/mxstbr ~/.config/karabiner >/dev/null 2>&1
 
 # Autocompletion settings
 # https://github.com/Phantas0s/.dotfiles/blob/master/zsh/completion.zsh
