1845b8ec716d71c191c0fe4968dd1496ea18c73b
Linkarzu <linkarzu@gmail.com>
2024-01-22 16:35:38 -0600

    added filename on top

--- neovim/nvim-lazyvim/lua/config/options.lua
+++ neovim/nvim-lazyvim/lua/config/options.lua
@@ -1,3 +1,5 @@
+-- Filename: ~/github/dotfiles-public/neovim/nvim-lazyvim/lua/config/options.lua
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
